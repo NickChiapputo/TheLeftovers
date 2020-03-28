9a6905f08728d335484f0a874b01eff4b14df407
NickChiapputo <chiapputo.nick@gmail.com>
2020-03-28 11:32:53 -0500

    Changed parameter parsing to chunk parsing.

--- src/js/edit-inventory.js
+++ src/js/edit-inventory.js
@@ -9,8 +9,18 @@ const server = http.createServer( ( req, res ) => {
 	res.statusCode = 200;
 	res.setHeader( 'Content-Type', 'text/plain' );
 
-	var query = url.parse( req.url, true ).query;
-	res.end( "Parameters we received: " + JSON.stringify( query ) + "\nURL we received: " + req.url );
+	let body = '';
+	req.on( 'data', chunk => {
+		body += chunk.toString();	// Convert buffer to string
+	} );
+
+	req.on( 'end', () => {
+		console.log( body );
+		res.end( "We parsed: " + body );
+	} );
+
+	// var query = url.parse( req.url, true ).query;
+	// res.end( "Parameters we received: " + JSON.stringify( query ) + "\nURL we received: " + req.url );
 } );
 
 server.listen( port, hostname, () => {
