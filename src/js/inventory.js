8475a1e5e756e4aa6ed5d4ed53234087f4ac0935
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-05 15:41:56 -0500

    Inventory Manager Powers
    Fully Function Edit and Delete

--- src/js/inventory.js
+++ src/js/inventory.js
@@ -1,24 +1,65 @@
+
 function getInventoryList() {
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.onreadystatechange = function() {
 		if( this.readyState == 4 && this.status == 200 ) 
 		{
-			var doc = document.getElementById( 'textarea-view' );
-		
+			//var doc = document.getElementById( 'textarea-view' );
+			var txt ="";
 			// Response is a JSON array of items
 			var obj = JSON.parse( this.responseText );
 			
 			var numItems = Object.keys( obj ).length;
-			doc.innerHTML = "Number of Inventory Items: " + numItems + "\n";
+//			<!--doc.innerHTML = "<p>Number of Inventory Items: " + numItems + "</p>";-->
 
 			var i;
+			txt += "<table style='width:100%; border: 1px solid black;'>" + "<tr><th style=' background-color: black; color: white;'>Item#</th><th style=' background-color: black; color: white;'>Ingredient</th><th style=' background-color: black; color: white;'>Count</th>"
 			for( i = 0; i < numItems; i++ )
 			{
 				var currItem = obj[ i ];
-				doc.innerHTML += "Item " + ( i + 1 ) + "\n" + 
-						"    Name:  " + currItem.name + "\n" + 
-						"    Count: " + currItem.count + "\n\n";
+				txt += "<tr><td style=' background-color: white; color: black;'>" + "Item " + ( i + 1 ) + "</td><td style=' background-color: white; color: black;'>" + 
+						"    Name:  " + currItem.name + "</td><td style=' background-color: white; color: black;'>" + 
+						"    Count: " + currItem.count + "</td></tr>";
 			}
+			txt += "</table>"
+			document.getElementById('invtxt').innerHTML = txt;
+		}
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+			console.log( "Request inventory status response: " + this.status );
+		}
+	};
+
+	// Send a GET request to 64.225.29.130/inventory/view
+	xmlHttp.open( "GET", "http://64.225.29.130/inventory/view", true );
+	xmlHttp.send();
+	var x = setTimeout(getInventoryList, 1000);
+}
+
+function getInventoryNames() {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 ) 
+		{
+			//var doc = document.getElementById( 'textarea-view' );
+			var txt ="";
+			var txt2 = "";
+			// Response is a JSON array of items
+			var obj = JSON.parse( this.responseText );
+			
+			var numItems = Object.keys( obj ).length;
+//			<!--doc.innerHTML = "<p>Number of Inventory Items: " + numItems + "</p>";-->
+
+			var i;
+			txt += "<table style='width:100%; border: 1px solid black;'>" + "<tr><th style=' background-color: black; color: white;'>Item#</th><th style=' background-color: black; color: white;'>Ingredient</th>"
+			for( i = 0; i < numItems; i++ )
+			{
+				var currItem = obj[ i ];
+				txt += "<tr><td style=' background-color: white; color: black;'>" +currItem.name+ "</td><td style=' background-color: white; color: black;'>" + 
+						"<button type='reset' onclick='deleteFormSubmit()'>Delete</button>" + "</td></tr>";
+			}
+			txt += "</table>"
+			document.getElementById('delinvtxt').innerHTML = txt;
 		}
 		else if( this.readyState == 4 && this.status != 200 )
 		{
@@ -116,9 +157,7 @@ function createFormSubmit()
 function deleteInventoryItem()
 {
 	var params = "name=" + document.getElementsByName( "name-delete" )[ 0 ].value;
-
 	var xmlHttp = new XMLHttpRequest();
-
 	xmlHttp.onreadystatechange = function() {
 		if( this.readyState == 4 && this.status == 200 )
 		{
