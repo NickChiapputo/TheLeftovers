4ad21808e059661d9998f016e948f09e83392deb
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-06 14:21:30 -0500

    updated menu with new text

--- src/js/menu.js
+++ src/js/menu.js
@@ -10,9 +10,32 @@ function getMenu()
 			var obj = JSON.parse( this.responseText );
 			
 			var numItems = Object.keys( obj ).length;
+      
 			doc.innerHTML = "Number of Menu Items: " + numItems + "\n";
 
-			doc.innerHTML = this.responseText;
+			var i;
+			for( i = 0; i < numItems; i++ )
+			{
+				var currItem = obj[ i ];
+				doc.innerHTML += "\nItem " + ( i + 1 ) + "\n";
+				for( var attr in currItem )
+				{
+					if( attr === "ingredients" )
+					{
+						doc.innerHTML += "    Ingredients:    " + ( currItem.hasIngredient[ 0 ] === 1 ? "(default) " : "          " ) + currItem[ attr ][ 0 ] + " - Uses " + currItem.ingredientCount[ 0 ] + "\n";
+
+						var j;
+						for( j = 1; j < Object.keys( currItem.ingredients ).length; j++ )
+							doc.innerHTML += "                    " + ( currItem.hasIngredient[ j ] === 1 ? "(default) " : "          " ) + currItem[ attr ][ j ] + " - Uses " + currItem.ingredientCount[ j ] + "\n";
+					}
+					else if( attr === "hasIngredient" || attr === "ingredientCount" || attr === "_id" )
+					{
+
+					}
+					else
+						doc.innerHTML += "    " + attr + ": " + currItem[ attr ] + "\n";
+				}
+			}
 
 			// var i;
 			// for( i = 0; i < numItems; i++ )
@@ -20,7 +43,7 @@ function getMenu()
 			// 	var currItem = obj[ i ];
 			// 	doc.innerHTML += 	"Item " + ( i + 1 ) + "\n" + 
 			// 						"    Name:  " + currItem.name + "\n" + 
-			// 						"    Price: " + currItem.price + "\n\n";
+			// 						"    Count: " + currItem.count + "\n\n";
 			// }
 		}
 		else if( this.readyState == 4 && this.status != 200 )
@@ -37,30 +60,72 @@ function getMenu()
 
 function createMenuItem()
 {
-	var blobFile = $( "#menu-item-create-picture" )[ 0 ].files[ 0 ];
-	var formData = new FormData();
-	formData.append( "fileToUpload", blobFile );
+	var formData = new FormData( createMenuItemForm );
+	formData.append( "fileToUpload", document.getElementById( "menu-item-create-picture" ).files[ 0 ] );
 
-	$.ajax( {
-		url: "http:64.225.29.130/menu/create",
-		type: "POST",
-		data: formData,
-		processData: false,
-		contentType: false,
-		success: function( response )
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 )
 		{
-			console.log( response );
-		},
+			var obj = JSON.parse( this.responseText );
+			var el = document.getElementById( "textarea-menu-create" );
 
-		error: function( jqXHR, textStatus, errorMessage ) {
-			console.log( errorMessage );
+			el.innerHTML = "Item Created:\n";
+			for( var attr in obj )
+				el.innerHTML += "    " + attr + ": " + obj[ attr ] + "\n";
+			console.log( this.responseText );
 		}
-	} );
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+			console.log( "Create menu item status response: " + this.status );
+		}
+	};
+
+	// Send a POST request to 64.225.29.130/menu/create
+	xmlHttp.open( "POST", "http://64.225.29.130/menu/create" );
+	xmlHttp.send( formData );
 }
 
 function createMenuItemSubmit()
 {
 	createMenuItem();
+
+	return false;
+}
+
+function deleteMenuItem()
+{
+	var params = "name=" + document.getElementsByName( "name-menu-delete" )[ 0 ].value;
+
+	var xmlHttp = new XMLHttpRequest();
+
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 )
+		{
+			console.log( this.responseText );
+
+			// Response is a JSON object
+			var obj = JSON.parse( this.responseText );
+
+			if( obj == null || obj.ok != 1 || obj.n != 1 )
+				document.getElementById( 'textarea-menu-delete' ).innerHTML = "Unable to delete item.\n";
+			else
+				document.getElementById( 'textarea-menu-delete' ).innerHTML = "Deleted Item\n";
+		}
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+			console.log( "Create inventory item status response: " + this.status );
+		}
+	};
+
+	// Send a POST request to 64.225.29.130/inventory/create with selected parameters in key-value format
+	xmlHttp.open( "POST", "http://64.225.29.130/menu/delete?" + params, true );
+	xmlHttp.send( params );
+}
+
+function deleteMenuItemFormSubmit()
+{
+	deleteMenuItem();
 
 	return false;
 }
