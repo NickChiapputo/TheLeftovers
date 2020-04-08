f91b2d4a5a2d37ec0ea3f9f5c12eb3c1baf634b8
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-08 16:32:11 -0500

    Add and Edit functionality

--- src/js/storage.js
+++ src/js/storage.js
@@ -7,7 +7,6 @@ const init = function (e)
 	var ingredients = document.querySelector("#food-ingredients");
 	var allergens = document.querySelector("#food-allergens");
 	var price = document.querySelector("#food-price");
-	var category = document.querySelector("#food-category");
 
 
     var xmlHttp = new XMLHttpRequest();
@@ -26,15 +25,22 @@ const init = function (e)
 					name.innerHTML+=d.name;
 					description.innerHTML+=d.description;
 					pic.innerHTML+="<div class=\"item-large-image\" style=\"background-image: url("+d.image+");max-height:70vh;min-height:70vh\"></div>";
-					d.ingredients.forEach(function(a){
-						ingredients.innerHTML+="<input type=\"checkbox\" id=\"topping\" name=\"topping\" value=\"topping\" checked>";
-						ingredients.innerHTML+="<label for=\"topping\">"+a+"</label><br>";
-
-					});
-					kcal.innerHTML+="kcal="+d.calories;
+					for(i=0; i< d.ingredients.length; i++)
+					{
+						if(d.hasIngredient[i]>0)
+						{
+							ingredients.innerHTML+="<input type=\"checkbox\" id=\"topping\" name=\"topping\" value=\"topping\" checked>";
+							ingredients.innerHTML+="<label for=\"topping\">"+d.ingredients[i]+":"+d.hasIngredient[i]+"</label><br>";
+						}
+						else
+						{
+							ingredients.innerHTML+="<input type=\"checkbox\" id=\"topping\" name=\"topping\" value=\"topping\">";
+							ingredients.innerHTML+="<label for=\"topping\">"+d.ingredients[i]+"</label><br>";
+						}
+					}
+					kcal.innerHTML+="Cal="+d.calories;
 					allergens.innerHTML+="Allergens="+d.allergens;
 					price.innerHTML+="Price=$"+d.price;
-					category.innerHTML+=d.category;
 
 					
 					
@@ -55,3 +61,41 @@ document.addEventListener('DOMContentLoaded', function()
 {
     init();
 });
+
+
+function deleteMenuItem()
+{
+	var params = "name=" + document.getElementsByName( "name-menu-delete" )[ 0 ].value;
+
+	var xmlHttp = new XMLHttpRequest();
+
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 )
+		{
+			console.log( this.responseText );
+
+			// Response is a JSON object
+			var obj = JSON.parse( this.responseText );
+
+			if( obj == null || obj.ok != 1 || obj.n != 1 )
+				document.getElementById( 'textarea-menu-delete' ).innerHTML = "Unable to delete item.\n";
+			else
+				document.getElementById( 'textarea-menu-delete' ).innerHTML = "Deleted Item\n";
+		}
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+			console.log( "Create inventory item status response: " + this.status );
+		}
+	};
+
+	// Send a POST request to 64.225.29.130/inventory/create with selected parameters in key-value format
+	xmlHttp.open( "POST", "http://64.225.29.130/menu/delete?" + params, true );
+	xmlHttp.send( params );
+}
+
+function deleteMenuItemFormSubmit()
+{
+	deleteMenuItem();
+
+	return false;
+}
