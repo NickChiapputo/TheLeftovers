85e658b25fc1c1c8efaa91c810301d1cbf1b0d9d
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-13 18:43:18 -0500

    Update kitchen-vieworder.js

--- src/js/kitchen-vieworder.js
+++ src/js/kitchen-vieworder.js
@@ -18,7 +18,7 @@ function getOrders()
 				var name=[];
 				var allergens;
 				var y=i+1;
-				txt+="<button type=\"button\" class=\"col btn btn-info\" data-toggle=\"collapse\" data-target=\"#order"+y+"\">Order"+y+"</button> ";
+				txt+="<button type=\"button\" id=\"btn"+y+"\" class=\"col btn btn-info\" data-toggle=\"collapse\" data-target=\"#order"+y+"\">Order"+y+"</button> ";
 				txt+="<div id=\"order"+y+"\" class=\"collapse\"> <div class=\"col text-box scrollable\">";
 				txt+="<p class=\"col-1\">Table:"+currItem.table+"</p>"
 				currItem.items.forEach(function (food) {
@@ -35,7 +35,7 @@ function getOrders()
 				});
 				if(currItem.notes!=undefined)
 					txt+="Note:"+currItem.notes;
-				txt+="</div><button type=\"button\" value=\"Notify Server\" onclick=\"sendMessage("+currItem.table+")\">Notify Server</button></div><div style=\"background-color:black;font-size:3px\">-</div> ";
+				txt+="</div><button type=\"button\" value=\"Notify Server\" onclick=\"findTable("+currItem.table+")\">Notify Server</button><button type=\"button\" style=\"background-color:lightgreen;\" value=\"Notify Server\" onclick=\"changeColor("+y+")\">Mark-Complete</button></div><div style=\"background-color:black;font-size:3px\">-</div> ";
 			}
 			
 			console.log( this.responseText );
@@ -54,12 +54,12 @@ function getOrders()
 
 }
 
-function sendMessage(table)
+function sendMessage(tableid)
 {
 	var params = {};
 	params['srcType']="kitchen";
-	params['dest']=table;
-	params['destType']="table";
+	params['dest']=tableid;
+	params['destType']="server";
 	params['request']="help";
 
 	var xmlHttp = new XMLHttpRequest();
@@ -74,7 +74,7 @@ function sendMessage(table)
 			var obj = JSON.parse( this.responseText );
 			var numItems = Object.keys( obj ).length;
 
-			alert("message sent");
+			alert("Server "+tableid+" was notified");
 			console.log( this.responseText );
 		}
 		else if( this.readyState == 4 && this.status != 200 )
@@ -89,3 +89,55 @@ function sendMessage(table)
 	xmlHttp.send(JSON.stringify(params));
 }
 
+function findTable(table)
+{
+
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 ) 
+		{
+//			var doc = document.getElementById( 'order-view-area' );
+
+			console.log( this.responseText );
+		
+			// Response is a JSON array of items
+			var obj = JSON.parse( this.responseText );
+			var numItems = Object.keys( obj ).length;
+			obj.forEach(function(item)
+			{
+				if(item.table==table)
+				{
+					
+					sendMessage(item._id);
+					return(false);
+				}
+			});
+			
+			console.log( this.responseText );
+		}
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+//			document.getElementById( 'textarea-orders-view' ).innerHTML = "Rewards accounts inventory status response: " + this.status;
+			console.log( "Rewards accounts inventory status response: " + this.status );
+		}
+	};
+
+	// Send a GET request to 64.225.29.130/inventory/view
+    xmlHttp.open( "GET", "http://64.225.29.130/tables/view", true );
+	xmlHttp.send();
+
+	
+}
+
+function changeColor(number)
+{
+	var btn="btn"+number;
+	if(document.getElementById(btn).style.backgroundColor=="green")
+	{
+		document.getElementById(btn).style.backgroundColor="#ffc107";
+	}
+	else
+	{
+		document.getElementById(btn).style.backgroundColor="green"
+	}
+}
