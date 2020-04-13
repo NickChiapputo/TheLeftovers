ce0e86f6b423012109304fd9b5119e01e672a523
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-13 18:00:52 -0500

    Update kitchen-vieworder.js

--- src/js/kitchen-vieworder.js
+++ src/js/kitchen-vieworder.js
@@ -35,7 +35,7 @@ function getOrders()
 				});
 				if(currItem.notes!=undefined)
 					txt+="Note:"+currItem.notes;
-				txt+="</div><button type=\"button\" value=\"Notify Server\" onclick=\"alert()\">Notify Server</button></div><div style=\"background-color:black;font-size:3px\">-</div> ";
+				txt+="</div><button type=\"button\" value=\"Notify Server\" onclick=\"sendMessage("+currItem.table+")\">Notify Server</button></div><div style=\"background-color:black;font-size:3px\">-</div> ";
 			}
 			
 			console.log( this.responseText );
@@ -51,10 +51,41 @@ function getOrders()
 	// Send a GET request to 64.225.29.130/inventory/view
     xmlHttp.open( "GET", "http://64.225.29.130/orders/view", true );
 	xmlHttp.send();
+
 }
 
-/*Contributed from https://www.w3schools.com/howto/howto_js_collapsible.asp*/
-var coll = document.getElementsByClassName("collapsible");
-var i;
+function sendMessage(table)
+{
+	var params = {};
+	params['srcType']="kitchen";
+	params['dest']=table;
+	params['destType']="table";
+	params['request']="help";
 
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 ) 
+		{
+//			var doc = document.getElementById( 'order-view-area' );
+
+			console.log( this.responseText );
+		
+			// Response is a JSON array of items
+			var obj = JSON.parse( this.responseText );
+			var numItems = Object.keys( obj ).length;
+
+			alert("message sent");
+			console.log( this.responseText );
+		}
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+//			document.getElementById( 'textarea-orders-view' ).innerHTML = "Rewards accounts inventory status response: " + this.status;
+			console.log( "Rewards accounts inventory status response: " + this.status );
+		}
+	};
+
+	// Send a GET request to 64.225.29.130/inventory/view
+    xmlHttp.open( "POST", "http://64.225.29.130/messages/send", true );
+	xmlHttp.send(JSON.stringify(params));
+}
 
