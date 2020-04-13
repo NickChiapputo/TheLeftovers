ea272666498677bcfa73632c52e5326eed489a91
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-13 03:47:55 -0500

    Working Edit Menu

--- src/js/manager-staff.js
+++ src/js/manager-staff.js
@@ -96,7 +96,7 @@ function getEmployeeList() {
 			var numItems = Object.keys( obj ).length;
 //			<!--doc.innerHTML = "<p>Number of Inventory Items: " + numItems + "</p>";-->
 			var i;
-			txt += "<table style='width:100%; border: 1px solid black;'>" + "<tr><th style=' background-color: black; color: white;'>EID</th><th style=' background-color: black; color: white;'>First</th><th style=' background-color: black; color: white;'>Middle</th><th style=' background-color: black; color: white;'>Last</th><th style=' background-color: black; color: white;'>Pin</th><th style=' background-color: black; color: white;'>Type</th><th style=' background-color: black; color: white;'>Shifts</th><th style=' background-color: black; color: white;'>Tips</th><th style=' background-color: black; color: white;'>Comps</th>"
+			txt += "<table style='width:100%; border: 1px solid black;'>" + "<tr><th style=' background-color: black; color: white;'>EID</th><th style=' background-color: black; color: white;'>First</th><th style=' background-color: black; color: white;'>Middle</th><th style=' background-color: black; color: white;'>Last</th><th style=' background-color: black; color: white;'>Pin</th><th style=' background-color: black; color: white;'>Type</th><th style=' background-color: black; color: white;'>Tips</th><th style=' background-color: black; color: white;'>Comps</th>"
 			for( i = 0; i < numItems; i++ )
 			{
 				var currItem = obj[ i ];
@@ -131,7 +131,6 @@ function getEmployeeList() {
                         +currItem.last +"</td></td><td style=' background-color: white; color: black;'>"
                         +currItem.pin +"</td><td style=' background-color: white; color: black;'>"
                         +currItem.type+"</td><td style=' background-color: white; color: black;'>"
-                        +shifts.join(",")+"</td><td style=' background-color: white; color: black;'>"
                         +currItem.tips+"</td><td style=' background-color: white; color: black;'>"
                         +currItem.comps+"</td></tr>";
 			}
