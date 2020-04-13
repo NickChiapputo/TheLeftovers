6eff11ac69562c2012346d542898c52592327d07
NickChiapputo <chiapputo.nick@gmail.com>
2020-04-13 01:28:29 -0500

    Implemented coupon create, view, delete, verify.

--- src/js/index.js
+++ src/js/index.js
@@ -1280,3 +1280,135 @@ function employeeCreateShiftSubmit( create )
 	return false;
 }
 /*******************************************/
+
+/*******************************************/
+/*            Employee Functions           */
+function getCoupons()
+{
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 ) 
+		{
+			var doc = document.getElementById( 'textarea-coupons-view' );
+
+			// Response is a JSON array of items
+			var obj = JSON.parse( this.responseText );
+			
+			var numItems = Object.keys( obj ).length;
+			doc.value = "Number of Coupons: " + numItems + "\n\n";
+		
+			var i;
+			for( i = 0; i < numItems; i++ )
+			{
+				var coupon = obj[ i ];
+				doc.value += 	"Coupon " + ( i + 1 ) + ":\n";
+
+				for( var attr in coupon )
+					doc.value += "    " + attr + " - " + coupon[ attr ] + "\n";
+			}
+		}
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+			document.getElementById( 'textarea-coupons-view' ).innerHTML = "Coupon query response: " + this.status;
+			console.log( "Coupon query response: " + this.status );
+		}
+	};
+
+	// Send a GET request to 64.225.29.130/coupons/view
+	xmlHttp.open( "GET", "http://64.225.29.130/coupons/view", true );
+	xmlHttp.send();
+}
+
+function createCouponSubmit()
+{
+	var coupon = {};
+
+	// Get coupon data
+	coupon[ "name" ] = document.getElementsByName( "create-coupon-name" )[ 0 ].value;
+	coupon[ "discount" ] = document.getElementsByName( "create-coupon-discount" )[ 0 ].value;
+	coupon[ "expiration" ] = document.getElementsByName( "create-coupon-expiration" )[ 0 ].value;
+	coupon[ "rewards" ] = document.getElementsByName( "create-coupon-description" )[ 0 ].value;
+
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 ) 
+		{
+			// var obj = JSON.parse( this.responseText );
+
+			document.getElementById( "textarea-coupons-create" ).innerHTML = "New Coupon: " + this.responseText;
+			
+			// for( var attr in obj )
+			// 	document.getElementById( "textarea-order-create" ).innerHTML += "    " + attr + ": "  + obj[ attr ];
+		
+		}
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+			document.getElementById( "textarea-coupons-create" ).innerHTML = "Status return: " + this.status; + "\n";
+		}
+	};
+
+	console.log( "Sending: " + JSON.stringify( coupon ) );
+
+	// Send a POST request to 64.225.29.130/employees/create
+	xmlHttp.open( "POST", "http://64.225.29.130/coupons/create", true );
+	xmlHttp.send( JSON.stringify( coupon ) );
+}
+
+function couponVerificationSubmit()
+{
+	var coupon = {};
+
+	// Get coupon information
+	coupon[ "_id" ] = document.getElementsByName( "coupon-verification-id" )[ 0 ].value;
+
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 ) 
+		{
+			document.getElementById( "textarea-coupon-verification" ).innerHTML = "Verification result: " + this.responseText;
+		}
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+			document.getElementById( "textarea-coupon-verification" ).innerHTML = "Status return: " + this.status; + "\n";
+		}
+	};
+
+	console.log( "Sending: " + JSON.stringify( coupon ) );
+
+	// Send a POST request to 64.225.29.130/coupon/verify
+	xmlHttp.open( "POST", "http://64.225.29.130/coupons/verify", true );
+	xmlHttp.send( JSON.stringify( coupon ) );
+}
+
+function couponDeleteSubmit()
+{
+	var params = {};
+	params[ "_id" ] = document.getElementsByName( "coupon-delete-id" )[ 0 ].value;
+
+	var xmlHttp = new XMLHttpRequest();
+
+	xmlHttp.onreadystatechange = function() {
+		if( this.readyState == 4 && this.status == 200 )
+		{
+			console.log( this.responseText );
+
+			// Response is a JSON object
+			var obj = JSON.parse( this.responseText );
+
+			if( obj == null || obj.ok != 1 || obj.n != 1 )
+				document.getElementById( 'textarea-coupon-delete' ).innerHTML = "Unable to delete coupon.\n";
+			else
+				document.getElementById( 'textarea-coupon-delete' ).innerHTML = "Deleted Coupon\n";
+		}
+		else if( this.readyState == 4 && this.status != 200 )
+		{
+			document.getElementById( 'textarea-coupon-delete' ).innerHTML = "Delete coupon status response: " + this.status;
+			console.log( "Delete coupon status response: " + this.status );
+		}
+	};
+
+	// Send a POST request to 64.225.29.130/coupons/delete
+	xmlHttp.open( "POST", "http://64.225.29.130/coupons/delete", true );
+	xmlHttp.send( JSON.stringify( params ) );
+}
+/*******************************************/
