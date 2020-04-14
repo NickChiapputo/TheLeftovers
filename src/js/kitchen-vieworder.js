033198714a12815b9e81a367a937a56af97fda6a
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-13 23:41:19 -0500

    up

--- src/js/kitchen-vieworder.js
+++ src/js/kitchen-vieworder.js
@@ -18,24 +18,30 @@ function getOrders()
 				var name=[];
 				var allergens;
 				var y=i+1;
-				txt+="<button type=\"button\" id=\"btn"+y+"\" class=\"col btn btn-info\" data-toggle=\"collapse\" data-target=\"#order"+y+"\">Order"+y+"</button> ";
-				txt+="<div id=\"order"+y+"\" class=\"collapse\"> <div class=\"col text-box scrollable\">";
-				txt+="<p class=\"col-1\">Table:"+currItem.table+"</p>"
-				currItem.items.forEach(function (food) {
-					var ingredients=[];
-					txt+="<p>Name:"+food.name+"</p>";
-					food.ingredients.forEach(function (ingredient){
-							ingredients.push(ingredient);
-					});
+				localStorage.setItem('btn'+y,"");
+				if(currItem.status!="complete"||currItem.status!="completed")
+				{
+					txt+="<button type=\"button\" id=\"btn"+y+"\" class=\"col btn btn-info\" data-toggle=\"collapse\" data-target=\"#order"+y+"\">Order"+y+"</button> ";
+					txt+="<div id=\"order"+y+"\" class=\"collapse\"> <div class=\"col text-box scrollable\">";
+					txt+="<p class=\"col-1\">Table:"+currItem.table+"</p>"
+					currItem.items.forEach(function (food) {
+						var ingredients=[];
+						txt+="<p>Name:"+food.name+"</p>";
+						food.ingredients.forEach(function (ingredient){
+								ingredients.push(ingredient);
+						});
 
-					if(ingredients.length!=0)
-					{
-						txt+="<p>Ingredients:"+ingredients.join(",")+"</p>"
-					}
-				});
-				if(currItem.notes!=undefined)
-					txt+="Note:"+currItem.notes;
-				txt+="</div><button type=\"button\" value=\"Notify Server\" onclick=\"findTable("+currItem.table+")\">Notify Server</button><button type=\"button\" style=\"background-color:lightgreen;\" value=\"Notify Server\" onclick=\"changeColor("+y+")\">Mark-Complete</button></div><div style=\"background-color:black;font-size:3px\">-</div> ";
+						if(ingredients.length!=0)
+						{
+							txt+="<p>Ingredients:"+ingredients.join(",")+"</p>"
+						}
+					});
+					if(currItem.notes!=undefined)
+						txt+="Note:"+currItem.notes;
+						var ide = "";
+						localStorage.setItem('btn'+y,currItem._id);
+					txt+="</div><button type=\"button\" value=\"Notify Server\" onclick=\"findTable("+currItem.table+")\">Notify Server</button><button type=\"button\" style=\"background-color:lightgreen;\" value=\"Notify Server\" onclick=\"changeColor("+y+")\">Mark-Complete</button><button type=\"button\" value=\"Notify Server\" style=\"background-color:red\" onclick=\"changeStatus("+y+")\">Clear</div><div style=\"background-color:black;font-size:3px\">-</div> ";
+				}
 			}
 			
 			console.log( this.responseText );
@@ -137,7 +143,7 @@ function changeColor(number)
 	}
 	else
 	{
-		document.getElementById(btn).style.backgroundColor="green"
+		document.getElementById(btn).style.backgroundColor="green";
 	}
 }
 
@@ -233,5 +239,39 @@ function help(managerid)
 	
 		// Send a GET request to 64.225.29.130/inventory/view
 		xmlHttp.open( "POST", "http://64.225.29.130/messages/send", true );
+		xmlHttp.send(JSON.stringify(params));
+}
+
+function changeStatus(count)
+{
+	var ide=localStorage.getItem('btn'+count);
+	localStorage.setItem('btn'+count,"");
+	var params = {};
+		params['_id'] = ide;
+		params ['status'] = 'complete';
+
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function() {
+			if( this.readyState == 4 && this.status == 200 ) 
+			{
+	//			var doc = document.getElementById( 'order-view-area' );
+	
+				console.log( this.responseText );
+			
+				// Response is a JSON array of items
+				var obj = JSON.parse( this.responseText );
+				var numItems = Object.keys( obj ).length;
+	
+				console.log( this.responseText );
+			}
+			else if( this.readyState == 4 && this.status != 200 )
+			{
+	//			document.getElementById( 'textarea-orders-view' ).innerHTML = "Rewards accounts inventory status response: " + this.status;
+				console.log( "Rewards accounts inventory status response: " + this.status );
+			}
+		};
+	
+		// Send a GET request to 64.225.29.130/inventory/view
+		xmlHttp.open( "POST", "http://64.225.29.130/orders/status", true );
 		xmlHttp.send(JSON.stringify(params));
 }
