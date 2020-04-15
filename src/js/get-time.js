4cabc563de3feafcd8f12327f6803476af251700
NickChiapputo <chiapputo.nick@gmail.com>
2020-04-15 01:03:48 -0500

    Changed time check interval to 5 seconds.

--- src/js/get-time.js
+++ src/js/get-time.js
@@ -1,34 +1,34 @@
-            /* credit w3schools for clock and addZero */
-            function clock() {
-                var date = new Date();
-                var am_pm = "pm"
-                var currentTime = date.getHours();
-                if (currentTime < 12) {
-                  am_pm = "am"
-                  if (currentTime == 0) {
-                    currentTime = 12
-                  }
-                }
-                else {
-                  if(currentTime!=12)
-                    currentTime = currentTime - 12;
-                }
-                currentTime += ":";
-                currentTime += addZero(date.getMinutes())
-                currentTime += am_pm;
-                document.getElementById("time").innerHTML = currentTime;
-                var x = setTimeout(clock, 1000);
-              }
-              function addZero(num) {
-                if (num < 10) {
-                  num = "0" + num;
-                }
-                return num;
-            }
-            function week(){
-              let date = new Date();
-              let currentweek = (date.getMonth()+1)+'/'+(date.getDate()-date.getDay())+'/'+date.getFullYear();
-              let nextweek = (date.getMonth()+1)+'/'+(date.getDate()+(6-date.getDay()))+'/'+date.getFullYear();
-              let duration = currentweek+'-'+nextweek;
-              document.getElementById("date").innerHTML=duration;
-            }
+/* credit w3schools for clock and addZero */
+function clock() {
+    var date = new Date();
+    var am_pm = "pm"
+    var currentTime = date.getHours();
+    if (currentTime < 12) {
+      am_pm = "am"
+      if (currentTime == 0) {
+        currentTime = 12
+      }
+    }
+    else {
+      if(currentTime!=12)
+        currentTime = currentTime - 12;
+    }
+    currentTime += ":";
+    currentTime += addZero(date.getMinutes())
+    currentTime += am_pm;
+    document.getElementById("time").innerHTML = currentTime;
+    var x = setTimeout(clock, 5000);
+  }
+  function addZero(num) {
+    if (num < 10) {
+      num = "0" + num;
+    }
+    return num;
+}
+function week(){
+  let date = new Date();
+  let currentweek = (date.getMonth()+1)+'/'+(date.getDate()-date.getDay())+'/'+date.getFullYear();
+  let nextweek = (date.getMonth()+1)+'/'+(date.getDate()+(6-date.getDay()))+'/'+date.getFullYear();
+  let duration = currentweek+'-'+nextweek;
+  document.getElementById("date").innerHTML=duration;
+}
