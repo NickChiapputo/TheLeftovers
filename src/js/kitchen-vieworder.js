85d82beddd86da6462f0e553d263213f87c55534
NickChiapputo <chiapputo.nick@gmail.com>
2020-04-15 13:49:10 -0500

    Changed help action to send to all logged in managers.

--- src/js/kitchen-vieworder.js
+++ src/js/kitchen-vieworder.js
@@ -66,7 +66,6 @@ function getOrders()
 		}
 		else if( this.readyState == 4 && this.status != 200 )
 		{
-			document.getElementById( 'textarea-orders-view' ).innerHTML = "Rewards accounts inventory status response: " + this.status;
 			console.log( "Rewards accounts inventory status response: " + this.status );
 		}
 	};
@@ -75,7 +74,6 @@ function getOrders()
     xmlHttp.open( "POST", "http://64.225.29.130/orders/get", true );
 	xmlHttp.send(JSON.stringify(params));
 	setTimeout(getOrders,5000);
-
 }
 
 function sendMessage(tableid)
@@ -90,8 +88,6 @@ function sendMessage(tableid)
 	xmlHttp.onreadystatechange = function() {
 		if( this.readyState == 4 && this.status == 200 ) 
 		{
-//			var doc = document.getElementById( 'order-view-area' );
-
 			console.log( this.responseText );
 		
 			// Response is a JSON array of items
@@ -103,25 +99,21 @@ function sendMessage(tableid)
 		}
 		else if( this.readyState == 4 && this.status != 200 )
 		{
-//			document.getElementById( 'textarea-orders-view' ).innerHTML = "Rewards accounts inventory status response: " + this.status;
-			console.log( "Rewards accounts inventory status response: " + this.status );
+			console.log( "Send Message response: " + this.status );
 		}
 	};
 
-	// Send a GET request to 64.225.29.130/inventory/view
+	// Send a POST request to 64.225.29.130/messages/send
     xmlHttp.open( "POST", "http://64.225.29.130/messages/send", true );
 	xmlHttp.send(JSON.stringify(params));
 }
 
 function findTable(table)
 {
-
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.onreadystatechange = function() {
 		if( this.readyState == 4 && this.status == 200 ) 
 		{
-//			var doc = document.getElementById( 'order-view-area' );
-
 			console.log( this.responseText );
 		
 			// Response is a JSON array of items
@@ -140,16 +132,13 @@ function findTable(table)
 		}
 		else if( this.readyState == 4 && this.status != 200 )
 		{
-//			document.getElementById( 'textarea-orders-view' ).innerHTML = "Rewards accounts inventory status response: " + this.status;
 			console.log( "Rewards accounts inventory status response: " + this.status );
 		}
 	};
 
-	// Send a GET request to 64.225.29.130/inventory/view
+	// Send a GET request to 64.225.29.130/tables/view
     xmlHttp.open( "GET", "http://64.225.29.130/tables/view", true );
 	xmlHttp.send();
-
-	
 }
 
 function changeColor(number)
@@ -171,51 +160,28 @@ function managerSearch()
 	xmlHttp.onreadystatechange = function() {
 		if( this.readyState == 4 && this.status == 200 ) 
 		{
-//			var doc = document.getElementById( 'order-view-area' );
-
 			console.log( this.responseText );
 		
 			// Response is a JSON array of items
 			var obj = JSON.parse( this.responseText );
 
 			var numItems = Object.keys( obj ).length;
-			let date = new Date();
-			let year = date.getFullYear();
-			let month = date.getMonth()+1
-			dt = date.getDate();
-		
-			if(dt < 10)
-			{
-				dt = '0' + dt;
-			}
-			if(month < 10)
-			{
-				month = '0' + month;
-			}
-		 
-			let today = (year+'-'+month+'-'+(dt))
 
-			obj.forEach(function(employee)
+			// Using for loop instead of foreach because it's cheaper
+			var i;
+			for( i = 0; i < numItems; i++ )
 			{
-				if(employee.type=='manager')
+				var employee = obj[ i ];
+
+				if( employee[ "type" ] === "manager" && employee[ "loggedIn" ] === 1 )
 				{
-					//alert(employee.first+" "+employee.last)
-					employee.shifts.forEach(function (shift){
-						//alert(shift.date);
-						if(shift.date==today)
-						{
-							help(employee._id)
-						}
-					} );
+					help( employee[ "_id" ] );
 				}
-			});
-			
-			console.log( this.responseText );
+			}
 		}
 		else if( this.readyState == 4 && this.status != 200 )
 		{
-//			document.getElementById( 'textarea-orders-view' ).innerHTML = "Rewards accounts inventory status response: " + this.status;
-			console.log( "Rewards accounts inventory status response: " + this.status );
+			console.log( "Get employees status response: " + this.status );
 		}
 	};
 
