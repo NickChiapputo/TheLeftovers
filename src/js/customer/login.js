b0c91ca742942e80edd3e1e4ff007d7171a095fb
NickChiapputo <chiapputo.nick@gmail.com>
2020-04-17 03:02:08 -0500

    Pull order into session storage on login.

--- src/js/customer/login.js
+++ src/js/customer/login.js
@@ -5,19 +5,34 @@ function initializeTable() {
 	// Verify that input is valid value
 	if( isNaN( enteredTableNumber ) || enteredTableNumber < 1 || enteredTableNumber > 20 )
 	{
+		// Inform user what is acceptable
 		alert( "Enter a table number between 1 and 20." );
+
+		// Break out of function
 		return;
 	}
 
 	// Query server with table number to ensure it is valid
-	var data = { "table" : "" };
+	var data = {};
 	data[ "table" ] = enteredTableNumber; 
-	var response = sendData( JSON.stringify( data ) );
+	var response = communicateWithServer( JSON.stringify( data ), "POST", "http://64.225.29.130/tables/search", false );
 
 	// If successful response, set table ID session storage key-value and navigate to index
 	// Otherwise, tell user table ID is invalid
 	if( response.status === 200 )
 	{
+		// Get order for table
+		var query = {};
+		query[ "table" ] = enteredTableNumber.toString();
+		var response = communicateWithServer( JSON.stringify( query ), "POST", "http://64.225.29.130/orders/get", false );
+
+		// If successful, set order session storage, else set order session storage to empty.
+		if( response.status === 200 )
+		{
+			sessionStorage.setItem( 'current_order', JSON.stringify( JSON.parse( response.responseText )[ 0 ] ) );
+			console.log( sessionStorage.getItem( 'current_order' ) );
+		}
+
 		// Set table ID session storage
 		sessionStorage.setItem( 'tableid', enteredTableNumber );
 
@@ -33,10 +48,10 @@ function initializeTable() {
 	}
 }
 
-function sendData( data )
+function communicateWithServer( data, request, url, asynchronous )
 {
 	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "POST", "http://64.225.29.130/tables/search", false );
+	xmlHttp.open( request, url, asynchronous );
 	xmlHttp.send( data );
 	return xmlHttp;
 }
