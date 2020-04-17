8441fa2dfde40ea0b9e4a7cbbb58fdbf715cb19b
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-17 17:33:08 -0500

    Manager Login

--- src/UnitTests/CouponsData.js
+++ src/UnitTests/CouponsData.js
@@ -7,7 +7,17 @@ const functions = {
     .get('http://64.225.29.130/coupons/view')
     .then(res => res.data)
     .catch(err => 'error'),
-    
+
+    appendCoupon: () =>
+    axios
+    .post('http://65.225.29.130/coupons/create',{
+        name:'UnitTest',
+        discount:'15',
+        expiration:'2020-05-16',
+        rewards:' UNIT TEST'
+    })
+    .then(res => res.data)
+    .catch(err => 'error')
 };
 
 module.exports = {functions};
--- src/UnitTests/CouponsData.test.js
+++ src/UnitTests/CouponsData.test.js
@@ -8,3 +8,12 @@ test('Makes sure the Coupons receives the get request', () => {
             expect(typeof(data)).toEqual("object");
         });
 });
+
+/*test('Makes sure the a new coupon was create',  () => {
+   // expect.assertions(1);
+    return functions.appendCoupon()
+        .then(data => {
+            console.log("Trying to connect to database")
+            expect(typeof(data)).toEqual("object");
+        })
+})*/
