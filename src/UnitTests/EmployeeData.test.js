044695444ac023b0769d1cc99cb3dc3158457bef
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-18 18:15:22 -0500

    Update

--- src/UnitTests/EmployeeData.test.js
+++ src/UnitTests/EmployeeData.test.js
@@ -1,5 +1,8 @@
 const {functions} = require('./EmployeeData')
+const communication = require('../js/server-communication')
 
+
+//Grabs object Data
 test('Makes sure the Employee receives the get request', () => {
    expect.assertions(1);
     return functions.fetchEmployees()
@@ -9,3 +12,61 @@ test('Makes sure the Employee receives the get request', () => {
         });
 });
 
+//Create
+test('Add a employee to the Employee database', () => {
+    console.log = jest.fn();
+    var query = {
+                    "first" : "UNIT",
+                    "middle" : "J",
+                    "last" : "Test",
+                    "type" : "manager",
+                };
+    var url = "http://64.225.29.130/employees/create"
+    var method = "POST";
+    var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+    console.log("Response status: "+response.status)
+    expect(response.status).toBe(200);
+});
+
+
+//Delete w/ invalid ID
+test('Cannot delete an employee with an invalid ID', () =>{
+    console.log = jest.fn();
+    var query = {
+                    "id" : ""
+                };
+    var url = "http://64.225.29.130/employees/delete"
+    var method = "POST";
+    var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+    console.log("Response text: "+response.status)
+    expect(response.status).toBe(400);
+})
+
+
+
+test('Login passes for an existing employee',() => {
+    console.log = jest.fn();
+    var query = {
+                    "_id" : "5e92d202d016150b8a388ac4",
+                    "pin" :"2568"
+                }
+                var url = "http://64.225.29.130/employees/login"
+                var method = "POST";
+                var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+                console.log("Response text: "+response.status)
+                expect(response.status).toBe(200);
+})
+
+test('Logout passes for an existing employee', () =>{
+    console.log = jest.fn();
+    var query = {
+                    "_id" : "5e92d202d016150b8a388ac4",
+                    "pin" : "2568"
+
+                }
+                var url = "http://64.225.29.130/employees/logout"
+                var method = "POST";
+                var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+                console.log("Response text: "+response.status)
+                expect(response.status).toBe(200);
+})
