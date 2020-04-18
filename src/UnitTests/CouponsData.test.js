0a89b8678da9ea398390e4818cd380e432e0b5d1
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-04-17 19:54:41 -0500

    update

--- src/UnitTests/CouponsData.test.js
+++ src/UnitTests/CouponsData.test.js
@@ -1,6 +1,7 @@
 const {functions} = require('./CouponsData')
-const {}
+const communication = require('../js/server-communication.js')
 
+//Get
 test('Makes sure the Coupons receives the get request', () => {
    expect.assertions(1);
     return functions.fetchCoupons()
@@ -10,11 +11,30 @@ test('Makes sure the Coupons receives the get request', () => {
         });
 });
 
-/*test('Makes sure the a new coupon was create',  () => {
-   // expect.assertions(1);
-    return functions.appendCoupon()
-        .then(data => {
-            console.log("Trying to connect to database")
-            expect(typeof(data)).toEqual("object");
-        })
-})*/
+//Create
+test('Add a coupon to the Coupon database', () => {
+    console.log = jest.fn();
+    var query = {
+                    "name" : "UNITTEST",
+                    "discount" :"15",
+                    "expiration" : "2020-05-16",
+                    "rewards" : "UNITTEST"
+                };
+    var url = "http://64.225.29.130/coupons/create"
+    var method = "POST";
+    var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+    console.log("Response status: "+response.status)
+    expect(response.status).toBe(200);
+});
+
+//Delete
+test('This should delete a coupon from the database',() => {
+    var query = {
+        "_id" : "5e9a456e14ce17091aab6a1a"
+    };
+    var url = "http://64.225.29.130/coupons/delete"
+    var method = "POST";
+    var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+    console.log("Response status: "+response.status)
+    expect(response.status).toBe(200);
+})
