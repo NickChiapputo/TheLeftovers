68b5e7c2939bb8d97d17d896127c5caf86e5c342
oopsies <ryanvanek2@gmail.com>
2020-04-18 23:05:02 -0500

    some more unit tests

--- src/UnitTests/EmployeeData.test.js
+++ src/UnitTests/EmployeeData.test.js
@@ -28,6 +28,16 @@ test('Add a employee to the Employee database', () => {
     expect(response.status).toBe(200);
 });
 
+//Create with blank data
+test('Cannot Add a employee without data to the Employee database', () => {
+    console.log = jest.fn();
+    var query = { };
+    var url = "http://64.225.29.130/employees/create"
+    var method = "POST";
+    var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+    console.log("Response status: "+response.status)
+    expect(response.status).toBe(400);
+});
 
 //Delete w/ invalid ID
 test('Cannot delete an employee with an invalid ID', () =>{
@@ -57,6 +67,24 @@ test('Login passes for an existing employee',() => {
                 expect(response.status).toBe(200);
 })
 
+<<<<<<< Updated upstream
+=======
+//Login Verification (bad login info)
+test('Login does not pass for bad login info',() => {
+    console.log = jest.fn();
+    var query = {
+                    "_id" : "2342562",
+                    "pin" :"2568"
+                }
+                var url = "http://64.225.29.130/employees/login"
+                var method = "POST";
+                var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+                console.log("Response text: "+response.status)
+                expect(response.status).toBe(400);
+})
+
+//Logout Verification
+>>>>>>> Stashed changes
 test('Logout passes for an existing employee', () =>{
     console.log = jest.fn();
     var query = {
@@ -69,4 +97,84 @@ test('Logout passes for an existing employee', () =>{
                 var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
                 console.log("Response text: "+response.status)
                 expect(response.status).toBe(200);
+<<<<<<< Updated upstream
 })
+=======
+})
+
+//Logout Verification
+test('Logout does for bad login info', () =>{
+    console.log = jest.fn();
+    var query = {
+                    "_id" : "4563456",
+                    "pin" : "2568"
+
+                }
+                var url = "http://64.225.29.130/employees/logout"
+                var method = "POST";
+                var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+                console.log("Response text: "+response.status)
+                expect(response.status).toBe(400);
+})
+
+//Create Shift
+test('Can create a shift for an employee with a valid id',() => {
+    console.log = jest.fn();
+    var query = {
+                    "_id" : "5e92d202d016150b8a388ac4",
+                    "date":"2020-14-12","start":"10:30","end":"14:30"
+
+                }
+                var url = "http://64.225.29.130/employees/shift/create"
+                var method = "POST";
+                var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+                console.log("Response text: "+response.status)
+                expect(response.status).toBe(200);
+})
+
+//Create Shift (BAD ID)
+test('Can create a shift for an employee with an invalid id',() => {
+    console.log = jest.fn();
+    var query = {
+                    "_id" : "5343453",
+                    "date":"2020-14-12","start":"10:30","end":"14:30"
+
+                }
+                var url = "http://64.225.29.130/employees/shift/create"
+                var method = "POST";
+                var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+                console.log("Response text: "+response.status)
+                expect(response.status).toBe(400);
+})
+
+
+//Delete Shift
+test('Can delete a shift for an employee with a valid id, that is scheduled',() => {
+    console.log = jest.fn();
+    var query = {
+        "_id" : "5e92d202d016150b8a388ac4",
+        "date":"2020-14-12","start":"10:30","end":"14:30"
+
+    }
+                var url = "http://64.225.29.130/employees/shift/remove"
+                var method = "POST";
+                var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+                console.log("Response text: "+response.status)
+                expect(response.status).toBe(200);
+})
+
+//Delete Shift
+test('Cannot delete a shift for an employee with an invalid id',() => {
+    console.log = jest.fn();
+    var query = {
+        "_id" : "534543534",
+        "date":"2020-14-12","start":"10:30","end":"14:30"
+
+    }
+                var url = "http://64.225.29.130/employees/shift/remove"
+                var method = "POST";
+                var response = communication.communicateWithServer(JSON.stringify(query), method, url, false);
+                console.log("Response text: "+response.status)
+                expect(response.status).toBe(400);
+})
+>>>>>>> Stashed changes
--- src/UnitTests/MenuItems.test.js
+++ src/UnitTests/MenuItems.test.js
@@ -2,6 +2,7 @@ const {functions} = require('./MenuItems')
 const communication = require('../js/server-communication.js')
 
 test('Makes sure the Menu receives the get request', () => {
+<<<<<<< Updated upstream
    expect.assertions(1);
     return functions.fetchMenu()
         .then(data => {
@@ -47,3 +48,70 @@ test('Reject stats on an item from the menu items database', () => {
     console.log("Response text: " + response.text)
     expect(response.status).toBe(500);
 });
+=======
+    expect.assertions(1);
+     return functions.fetchMenu()
+         .then(data => {
+             console.log("Trying to connect to database")
+             expect(typeof(data)).toEqual("object");
+         });
+ });
+
+ // test('Can create a shift for an employee with a valid id',() => {
+ //                 const file = new File([''],'http://64.225.29.130/img/yd7b6zdowsr41.jpg',{
+ //                     type: 'text/plain',
+ //                     lastModified:new Date()
+ //                 })
+ //                 var formData = new FormData();
+ //                 formData.set("menu-item-create-name","UNITTEST");
+ //                 formData.set("menu-item-create-description","UNITTEST");
+ //                 formData.set("menu-item-create-category","drink");
+ //                 formData.set("menu-item-create-calories","100");
+ //                 formData.set("menu-item-create-price","2.99");
+ //                 formData.set("menu-item-create-ingredient-1","1");
+ //                 formData.set("menu-item-create-has-ingredient-1","1");
+ //                 formData.set("menu-item-create-ingredient-count-1","1");
+ //                 formData.append("fileToUpload",file)
+ //                 var url = "http://64.225.29.130/menu/create"
+ //                 var method = "POST";
+ //                 var response = communication.communicateWithServer(formData, method, url, false);
+ //                 console.log("Response text: "+response.status)
+ //                 expect(response.status).toBe(200);
+ // })
+ //delete an empty menu item
+ test('Delete an empty item from the menu items database', () => {
+     console.log = jest.fn();
+     var menuItem = { "name":"?" };
+
+     var url = "http://64.225.29.130/menu/delete"
+     var method = "POST";
+     var response = communication.communicateWithServer(JSON.stringify(menuItem), method, url, false);
+     console.log("Response text: " + response.text)
+     expect(response.status).toBe(200);
+ });
+
+
+ //get stat on a menu item
+ test('Get stats on an item from the menu items database', () => {
+     console.log = jest.fn();
+     var menuItem = { "name":"Water" };
+
+     var url = "http://64.225.29.130/menu/stats"
+     var method = "POST";
+     var response = communication.communicateWithServer(JSON.stringify(menuItem), method, url, false);
+     console.log("Response text: " + response.text)
+     expect(response.status).toBe(200);
+ });
+
+ //reject getting stat on a bad menu item
+ test('Reject stats on an item from the menu items database', () => {
+     console.log = jest.fn();
+     var menuItem = { "name":"bad" };
+
+     var url = "http://64.225.29.130/menu/stats"
+     var method = "POST";
+     var response = communication.communicateWithServer(JSON.stringify(menuItem), method, url, false);
+     console.log("Response text: " + response.text)
+     expect(response.status).toBe(500);
+ });
+>>>>>>> Stashed changes
